9ee05b712359d833b681b1c1f949e17a1549fde9
andres.mera <feli041089@gmail.com>
2025-03-15 10:00:42 -0500

    code 01: Creating the employee table component.

---
+++ src/app/components/table-employee/table-employee.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-table-employee',
+  templateUrl: './table-employee.component.html',
+  styleUrl: './table-employee.component.scss'
+})
+export class TableEmployeeComponent {
+
+}
