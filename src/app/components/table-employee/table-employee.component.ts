c082f42a038a1214d03d4543aa36b9b9f84a84a3
andres.mera <feli041089@gmail.com>
2025-03-15 16:51:02 -0500

    code 01: Adding the modal to display employee detail data.

--- src/app/components/table-employee/table-employee.component.ts
+++ src/app/components/table-employee/table-employee.component.ts
@@ -9,10 +9,25 @@ import { Employee } from '../../models/employee';
 })
 export class TableEmployeeComponent implements OnInit {
   employees: Employee[] = [];
+  employee: Employee = {
+    id: 0,
+    name: '',
+    salary: 0,
+    age: 0,
+    image: '',
+    employeeAnnualSalary: 0
+  };
 
-  constructor(private employeeService: EmployeeService, private changeDetectorRef: ChangeDetectorRef) { }
+  constructor(
+    private employeeService: EmployeeService,
+    private changeDetectorRef: ChangeDetectorRef
+  ) { }
 
   ngOnInit(): void {
+    this.getEmployeesList();
+  }
+
+  public getEmployeesList(): void {
     this.employeeService
       .getListEmployee()
       .subscribe({
@@ -21,6 +36,26 @@ export class TableEmployeeComponent implements OnInit {
             if (datos) {
               console.log(datos);
               this.employees.push({ id: datos.id, name: datos.name, salary: datos.salary, age: datos.age, image: datos.image, employeeAnnualSalary: datos.employeeAnnualSalary });
+              this.changeDetectorRef.detectChanges(); // Fuerza la detección de cambios
+            }
+          },
+        error: (err) => {
+          console.error('Error al obtener datos:', err);
+        }
+      });
+    this.employees.push({ id: 1, name: 'Tiger Nixon', salary: 320800, age: 61, image: '', employeeAnnualSalary: 3849600 });
+    this.employees.push({ id: 2, name: 'Garrett Winters', salary: 170750, age: 63, image: '', employeeAnnualSalary: 2049000 });
+  }
+
+  public getEmployee(id: number): void {
+    this.employeeService
+      .getByIdEmployee(id)
+      .subscribe({
+        next:
+          (datos: Employee) => {
+            if (datos) {
+              console.log(datos);
+              this.employee = datos;
               // this.changeDetectorRef.detectChanges(); // Fuerza la detección de cambios
             }
           },
@@ -28,8 +63,20 @@ export class TableEmployeeComponent implements OnInit {
           console.error('Error al obtener datos:', err);
         }
       });
+  }
 
-    this.employees.push({ id: 1, name: 'Maria', salary: 120000, age: 25, image: '', employeeAnnualSalary: 240000 });
-    this.employees.push({ id: 12, name: 'Jsoe', salary: 130000, age: 50, image: '', employeeAnnualSalary: 6475000 });
+  public openModal(id: number): void {
+    const modeDiv = document.getElementById('myModal');
+    if (modeDiv != null) {
+      modeDiv.style.display = 'block';
+    }
+    this.getEmployee(id);
+  }
+
+  public closeModal(): void {
+    const modeDiv = document.getElementById('myModal');
+    if (modeDiv != null) {
+      modeDiv.style.display = 'none';
+    }
   }
 }
